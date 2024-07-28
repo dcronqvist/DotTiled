7e325ea95d52c2ba70ce61fda95099b8ee410ec5
Daniel Cronqvist <dancro99@gmail.com>
2024-07-28 23:50:16 +0200

    Add initial benchmarking project

---
+++ DotTiled.Benchmark/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace MyBenchmarks
+{
+  public class MapLoader
+  {
+    public MapLoader()
+    {
+    }
+
+    [Benchmark]
+    public DotTiled.Map LoadWithDotTiled()
+    {
+      throw new NotImplementedException();
+    }
+
+    [Benchmark]
+    public TiledLib.Map LoadWithTiledLib()
+    {
+      throw new NotImplementedException();
+    }
+  }
+
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      //var summary = BenchmarkRunner.Run<Md5VsSha256>();
+    }
+  }
+}
