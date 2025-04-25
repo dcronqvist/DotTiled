57b903bea1dd9cbe24d8cc36c0f4a026c76314ac
Daniel Cronqvist <dancro99@gmail.com>
2025-04-26 01:06:00 +0200

    Add MonoGame example project

---
+++ src/DotTiled.Examples/DotTiled.Example.MonoGame/Content/world-tileset.tsx
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="roguelikeSheet_transparent" tilewidth="16" tileheight="16" spacing="1" tilecount="1767" columns="57">
+ <image source="kenney_roguelike-rpg-pack/roguelikeSheet_transparent.png" width="968" height="526"/>
+ <wangsets>
+  <wangset name="Stuff" type="mixed" tile="-1">
+   <wangcolor name="Brown path" color="#ff0000" tile="692" probability="1"/>
+   <wangcolor name="Water" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="2" wangid="0,0,2,2,2,0,0,0"/>
+   <wangtile tileid="3" wangid="0,0,2,2,2,2,2,0"/>
+   <wangtile tileid="4" wangid="0,0,0,0,2,2,2,0"/>
+   <wangtile tileid="57" wangid="2,2,2,0,2,2,2,2"/>
+   <wangtile tileid="58" wangid="2,2,2,2,2,0,2,2"/>
+   <wangtile tileid="59" wangid="2,2,2,2,2,0,0,0"/>
+   <wangtile tileid="60" wangid="2,2,2,2,2,2,2,2"/>
+   <wangtile tileid="61" wangid="2,0,0,0,2,2,2,2"/>
+   <wangtile tileid="114" wangid="2,0,2,2,2,2,2,2"/>
+   <wangtile tileid="115" wangid="2,2,2,2,2,2,2,0"/>
+   <wangtile tileid="116" wangid="2,2,2,0,0,0,0,0"/>
+   <wangtile tileid="117" wangid="2,2,2,0,0,0,2,2"/>
+   <wangtile tileid="118" wangid="2,0,0,0,0,0,2,2"/>
+   <wangtile tileid="404" wangid="0,0,1,0,1,0,1,0"/>
+   <wangtile tileid="405" wangid="1,0,0,0,1,0,1,0"/>
+   <wangtile tileid="406" wangid="0,0,1,0,1,0,0,0"/>
+   <wangtile tileid="407" wangid="0,0,0,0,1,0,1,0"/>
+   <wangtile tileid="408" wangid="1,0,0,0,1,0,0,0"/>
+   <wangtile tileid="461" wangid="1,0,1,0,1,0,0,0"/>
+   <wangtile tileid="462" wangid="1,0,1,0,0,0,1,0"/>
+   <wangtile tileid="463" wangid="1,0,1,0,0,0,0,0"/>
+   <wangtile tileid="464" wangid="1,0,0,0,0,0,1,0"/>
+   <wangtile tileid="465" wangid="0,0,1,0,0,0,1,0"/>
+   <wangtile tileid="632" wangid="1,0,0,0,0,0,0,0"/>
+   <wangtile tileid="633" wangid="0,0,0,0,1,0,0,0"/>
+   <wangtile tileid="689" wangid="0,0,0,0,0,0,1,0"/>
+   <wangtile tileid="690" wangid="0,0,1,0,0,0,0,0"/>
+   <wangtile tileid="691" wangid="1,0,1,0,1,0,1,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
